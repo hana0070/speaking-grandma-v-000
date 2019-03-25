b18510203ffb943a9397d74de129a547867cd872
HANA LEANDER <hana0070@gmail.com>
2019-03-25 17:06:04 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  if phrase.uppercase
+  if phrase.uppercase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
   elsif
     puts "NO, NOT SINCE 1938!"
