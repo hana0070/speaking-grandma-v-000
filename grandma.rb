fc9eec93e8923184109683b8a3e1214ea8070bf4
HANA LEANDER <hana0070@gmail.com>
2019-03-25 17:13:18 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,7 +17,6 @@ def speak_to_grandma(phrase)
     puts "HUH?! SPEAK UP, SONNY!"
   elsif phrase == phrase.upcase
     puts "I LOVE YOU TOO PUMPKIN!"
-
   else
     puts "NO, NOT SINCE 1938!"
   end
